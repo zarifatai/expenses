aadabfb9f65411cfe9332efd6095ea7c08f541c3
Zarif Atai <z.atai@protonmail.com>
2023-04-18 20:53:10 +0200

    Added saved spent and summary commands

--- cli.py
+++ cli.py
@@ -1,6 +1,19 @@
 import argparse
 
+
 def load_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--periods", default=5, help="Submit number of periods you want to retrieve")
+    parser.add_argument(
+        "cmd",
+        choices=["saved", "spent", "summary"],
+        nargs="?",
+        default="summary",
+        )
+    parser.add_argument(
+        "-p",
+        "--periods",
+        default=5,
+        type=int,
+        help="Submit number of periods you want to retrieve",
+    )
     return parser.parse_args()
