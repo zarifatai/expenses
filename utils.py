3b8637f7d1c115236d488d25f85cc6319682e239
Zarif Atai <z.atai@protonmail.com>
2023-04-18 20:54:59 +0200

    Added function for every command

---
+++ utils.py
@@ -0,0 +1,38 @@
+from termcolor import colored
+
+
+def print_colored(amt, thld=50):
+    if amt > thld:
+        print(colored(amt, "green"))
+    elif amt < thld:
+        print(colored(amt, "red"))
+    else:
+        print(amt)
+
+def validate_periods(expenses, periods):
+    if periods <= len(expenses) or period != -1:
+        return expenses.iloc[-periods:]
+    return expenses
+
+
+def saved_cmd(expenses, periods):
+    savings = expenses["saved"].round(2)
+    savings = validate_periods(savings, periods)
+    [print_colored(x) for x in list(savings)]
+    print(f"\nTotal saved past {periods} periods: ", end="")
+    print_colored(round(sum(savings), 2))
+
+
+def spent_cmd(expenses, periods):
+    spent = expenses["expenses"].round(2)
+    spent = validate_periods(spent, periods)
+    [print(x) for x in list(spent)]
+    print(f"\nTotal spent past {periods} periods: ", end="")
+    print(round(sum(spent), 2))
+
+
+def summary_cmd(expenses, periods):
+    subset = expenses[["expenses", "saved"]].round(2)
+    subset = validate_periods(subset, periods)
+    print(f"Number of periods: {len(subset)}\n")
+    print(subset.describe().iloc[[1, 2, 3, 7]])
