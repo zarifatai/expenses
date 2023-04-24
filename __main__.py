971ff042e9d46c66461115074923baa48c004144
Zarif Atai <z.atai@protonmail.com>
2023-04-25 00:08:41 +0200

    Removed data utils file

--- __main__.py
+++ __main__.py
@@ -2,14 +2,11 @@ import json
 import os
 
 import cli
-import data_utils
 import utils
 
 args = cli.load_args()
-if not os.path.isfile("exp.json"):
-    data_utils.file_to_json()
 with open("exp.json") as f:
-    expenses = data_utils.load_data(json.load(f), "inkomsten")
+    expenses = utils.load_data(json.load(f), ["inkomsten"])
 
 commands = {
     "saved": utils.saved_cmd,
@@ -20,6 +17,3 @@ commands = {
 
 ret = commands[args.cmd](expenses, args)
 
-print(expenses)
-print("Income names:", ret[1])
-print(ret[0])
