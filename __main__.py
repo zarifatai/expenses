36aafd28ef29bf2e6630b8b7c092d8501e5e7932
Zarif Atai <z.atai@protonmail.com>
2023-05-13 01:41:49 +0200

    Separated mutation commands from view commands due to different input parameters

--- __main__.py
+++ __main__.py
@@ -9,14 +9,21 @@ args = cli.load_args()
 income_categories = ["inkomsten"]
 expenses, calculated_amounts = utils.read_file("exp.json", income_categories)
 
-commands = {
+view_commands = {
     "saved": utils.saved_cmd,
     "summary": utils.summary_cmd,
     "spent": utils.spent_cmd,
+}
+
+mutation_commands = {
     "new": cli.new_period,
 }
 
-ret = commands[args.cmd](expenses, args)
+if args.cmd in view_commands:
+    ret = view_commands[args.cmd](calculated_amounts, args)
+elif args.cmd in mutation_commands:
+    ret = mutation_commands[args.cmd](expenses, args)
+
 
 if args.cmd == "new":
     new_period, income_categories = ret
